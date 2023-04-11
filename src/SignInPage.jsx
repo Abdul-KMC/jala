fcc9c3650be1b36dc31928b70f80947123840adb
Abdul-KMC <75877950+Abdul-KMC@users.noreply.github.com>
2023-04-11 17:41:54 +0530

    Added css to login page

--- src/SignInPage.jsx
+++ src/SignInPage.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import './SignInPage.css';
 
 const SignInPage = () => {
   const [email, setEmail] = useState('');
@@ -22,33 +23,34 @@ const SignInPage = () => {
     
     <div className='page'>
       <div className="disp">
-         <h1>JALA Academy</h1>
-         <h3 className="log_cred">Login Credentials</h3>
-         <p className="email">Email : abdul@jalaacademy.com</p>
-         <p className="password">Password : abc@12345</p>
+         <p className='title'>JALA Academy</p>
+         <p className="log_cred">Login Credentials</p>
+         <p className="cred">Email : abdul@jalaacademy.com</p>
+         <p className="cred">Password : abc@12345</p>
          <p className="note">Learn Everything on Real-Time Scenarios</p>
       </div>
       <div className="SIP">
-      <p>Sign In</p>
+      <p className='si'>Sign In</p>
       <form onSubmit={handleSignIn}>
         <label>
           Email:
-          <input type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
+          <input className='crd' type="email" value={email} onChange={(e) => setEmail(e.target.value)} required />
         </label>
         <br />
         <label>
           Password:
-          <input type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
+          <input className='crd' type="password" value={password} onChange={(e) => setPassword(e.target.value)} required />
         </label>
         <br />
         <label>
-          <input type="checkbox" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />
-          Remember me
+          <input className='rm' type="checkbox" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} />
+          Remember Me
         </label>
         <br />
-        <button type="submit">Sign In</button>
+        <button className='sib' type="submit">Sign In</button>
         <br />
-        <button onClick={() => setShowForgotPassword(true)}>Forgot password</button>
+        <p className="or">OR</p>
+        <button className='fp' onClick={() => setShowForgotPassword(true)}>Forgot password</button>
       </form>
       {showForgotPassword && (
         <form onSubmit={handleForgotPassword}>
